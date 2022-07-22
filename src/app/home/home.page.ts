e54e1c1233e2bda34426a42f16f2b0c6dff6cc7a
PedroCandia <pedro.candiavz@pertendo.com>
2022-07-22 00:41:08 -0500

    [dev] *Dark Mode

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,10 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage{
+  toggleDarkMode;
+  darkMode = localStorage.getItem('darkMode');
 
   constructor() {}
 
   toSeccion(secccion){
     document.querySelector(`#${secccion}`).scrollIntoView({behavior:'smooth'});
+  }
+
+  toggleTheme(ev){
+    if(ev.detail.checked){
+      document.body.setAttribute('color-theme', 'dark');
+      localStorage.setItem('darkMode', 'true');
+    } else {
+      document.body.setAttribute('color-theme', 'light');
+      localStorage.setItem('darkMode', 'false');
+    }
   }
 }
