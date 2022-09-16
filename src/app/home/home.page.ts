27cd2e6eb3678207f3b194321236f0fbefdb6117
PedroCandia <pedro.candiavz@pertendo.com>
2022-09-16 01:41:10 -0500

    [dev] technologies, tech desing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,6 +9,20 @@ export class HomePage{
   toggleDarkMode;
   darkMode = localStorage.getItem('darkMode');
 
+  technologies1 =  [
+    {attribute: 'HTML', srcImg: 'assets/icon/html.png'},
+    {attribute: 'CSS', srcImg: 'assets/icon/css.png'},
+    {attribute: 'JavaScript', srcImg: 'assets/icon/js.png'},
+    {attribute: 'Angular', srcImg: 'assets/icon/angular.png'},
+  ];
+
+  technologies2 =  [
+    {attribute: 'Ionic', srcImg: 'assets/icon/ionic.png'},
+    {attribute: 'Git', srcImg: 'assets/icon/git.png'},
+    {attribute: 'NodeJS', srcImg: 'assets/icon/nodejs.png'},
+    {attribute: 'Firebase', srcImg: 'assets/icon/firebase.png'},
+  ];
+
   constructor() {}
 
   toSeccion(secccion){
